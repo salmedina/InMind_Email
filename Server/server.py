fbe1f18045edcecbdf2b595bc4df71be02fb51f6
Scott <bcatctr@gmail.com>
2017-05-07 16:09:47 -0400

    new server

--- Server/server.py
+++ Server/server.py
@@ -16,9 +16,10 @@ import os
 import pprint
 import json
 import jsonrpc
-import jsonrpclib
+#import jsonrpclib
 import argparse
 import nltk
+import nltk.data
 import spacy
 from TextSummarizer import TextSummarizer
 from simplejson import loads
@@ -32,32 +33,39 @@ import numpy as np
 import pickle
 import fasttext
 
-ner = SpaCyNER(modelPath)            #modelPath  : need to modify
-
+# pre-load models
+ner = SpaCyNER("./NER_Model")            #modelPath  : need to modify
+fastTextClassifier = fasttext.load_model('model.bin')
 nlp = spacy.load('en')
-
 annotator=Annotator()
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 # Load summarization globals
 summarizer = TextSummarizer()
 
 # Load classifier globals
-count_vect = pickle.load( open( "count_vect", "rb" ) )
-tfidf_transformer = pickle.load( open( "tfidf_transformer", "rb" ) )
-clf = pickle.load( open( "model.clf", "rb" ) )
-clf1 = pickle.load( open( "model1.clf", "rb" ) )
+#count_vect = pickle.load( open( "count_vect", "rb" ) )
+#tfidf_transformer = pickle.load( open( "tfidf_transformer", "rb" ) )
+#clf = pickle.load( open( "model.clf", "rb" ) )
+#clf1 = pickle.load( open( "model1.clf", "rb" ) )
 
 # Establish connection with the SCNLP server
-server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(), jsonrpc.TransportTcpIp(addr=("holbox.lti.cs.cmu.edu", 9000)))
+#server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(), jsonrpc.TransportTcpIp(addr=("holbox.lti.cs.cmu.edu", 9000)))
 #server = jsonrpclib.Server("http://localhost:9000")
 # Test connection with the SCNLP server
-result = loads(server.parse("This is the hello world testing text for standord nlp"))
-if result != '':
-    print 'Established connection to SCNLP service'
+#result = loads(server.parse("This is the hello world testing text for standord nlp"))
+#if result != '':
+#    print 'Established connection to SCNLP service'
 
 # ===================================================================
 # AUXILIARY FUNCTIONS
 # ===================================================================
+
+def splitSentence(paragraph):
+    sentences = tokenizer.tokenize(paragraph)  
+    return sentences
+
+
 
 def time_match(strg, search=re.compile(r'[^0-9.:;\'\"]').search):
     return not bool(search(strg))
@@ -128,14 +136,14 @@ def solve_email_corref(text, i_replacement, you_replacement):
     
 def preprocess_email(text, i_replacement, you_replacemnt):
     #extract thread here
-    latestEmailBody = Extractor.extract(text)[0][0]
+    latestEmailBody = Extractor.extract(text)[0][0][1]
     
     processed_text = solve_email_corref(latestEmailBody, i_replacement,you_replacemnt)
     return processed_text
 
     
 @app.route('/parse', methods = ['GET', 'POST'])
-def distract():
+def parse():
     global ner
     global annotator
     
@@ -146,68 +154,40 @@ def distract():
     output['what'] = ''
     output['when'] = ''
     output['where'] = ''
-    output['type'] = -1
     output['sender'] = ''
     
     print 'Processing request'
     print 'Loading request'
+    if request.method == 'POST':
+        data = json.loads(request.form['data'])
+        print data
+        #data  fields:   subject  body   sender  user_name
+        print 'Normalizing request fields'
+        for key in data.keys():
+            data[key.lower()] = data.pop(key)
+        keys = data.keys()
 
-    print request.data
-    print len(request.data)
-    print request.form['data']
-    data = json.loads(request.form['data'])
-    #data  fields:   subject  body   sender  user_name
-    print 'Normalizing request fields'
-    for key in data.keys():
-        data[key.lower()] = data.pop(key)
-    keys = data.keys()
-    
-    print 'Validating request fields'
-    if "body" in keys:
-        print 'Found Body in the request'
-        body = data["body"]
-    else:
-        return "NO BODY!"
-    
-    if "sender" in keys:
-        print 'Found Sender in the request'
-        output["who"] = data["sender"]
-        
-    if "subject" in keys:
-        print 'Found Subject in the request'
-        output["what"] = data["subject"]
-    
-    print 'Parsing body through SCNLP'
-    result = loads(server.parse(body))
+        body = ''
+        print 'Validating request fields'
+        if "body" in keys:
+            print 'Found Body in the request'
+            body = data["body"]
+        else:
+            return "NO BODY!"
 
-    print 'Looking for time-entity within sentences'
-    foundTimeEntity = False
-    for s in result['sentences']:
-        for w in s['words']:
-            if 'Timex' in w[1].keys():
-                print 'Found time-entity within a sentence.'
-                foundTimeEntity = True
+        if "sender" in keys:
+            print 'Found Sender in the request'
+            output["sender"] = data["sender"]
+
+        if "subject" in keys:
+            print 'Found Subject in the request'
+            output["what"] = data["subject"]
+
     
     print 'Classifying e-mail'
-    #X_test_counts = count_vect.transform([body])
-    #X_test_tfidf = tfidf_transformer.transform(X_test_counts)
-    #predicted = clf.predict(X_test_tfidf)
-    #predicted1 = clf1.predict(X_test_tfidf)
-
-    #if predicted[0] == 1 or foundTimeEntity:
-        # Timed event e-mail class
-    #    output["type"] = 1
-    #elif predicted1[0] == 1:
-        # Meeting event but non-timed
-    #   output["type"] = 2
-    #else:
-        # Any other e-mail class
-    #    output['type'] = 0
-    #we first need to do body extraction by the thread extractor
-    #and then get the body = string 
     
     body = preprocess_email(body, data["sender"], data["user_name"])
-    fastTextClassifier = fasttext.load_model('model_bin')
+
     #text = [body]
     label = int(fastTextClassifier.predict([body])[0][0][-1])
     
@@ -233,24 +213,54 @@ def distract():
     preIdx ={'TIME':-2,'DATE':-2,'LOCATION':-2,'NAME':-2,'ORGANIZATION':-2}
     for i in range(len(nerRst)):
         if nerRst[i][1] in searchDict:
-            if i==(preIdx[nerRst[i][1]]+1):
-                searchDict[nerRst[i][1]].append(preIdx[nerRst[i][0])
+            if i == (preIdx[nerRst[i][1]] + 1):
+                searchDict[nerRst[i][1]].append(nerRst[i][0])
             else:
-                if len(searchDict[item[1]])!=0:
-                    rstDict[nerRst[i][1]].append(' '.join(searchDict[item[1]]))
-                searchDict[item[1]] = [preIdx[nerRst[i][0]]
+                if len(searchDict[nerRst[i][1]]) != 0:
+                    rstDict[nerRst[i][1]].append(' '.join(searchDict[nerRst[i][1]]))
+                searchDict[nerRst[i][1]] = [nerRst[i][0]]
             preIdx[nerRst[i][1]] = i
-    
+
     for k,v in searchDict.items():
         if len(v)!=0:
             rstDict[k].append(' '.join(v))
             
     output['what'] = ','.join(rstDict['ORGANIZATION'])
     output['who'] = ','.join(rstDict['NAME'])
-    output['where'] = ',' join(rstDict['LOCATION'])
+    output['where'] = ','.join(rstDict['LOCATION'])
     output['when'] = ','.join(rstDict['TIME']) + '\t' + ','.join(rstDict['DATE'])
     
+    #use practnlptools
+    sents = splitSentence(body)
+    srlRst = []
+    for oneSent in sents:
+        oneSent = oneSent.replace('\n',' ')
+        thisSrl = annotator.getAnnotations(oneSent)['srl']
+        for t in thisSrl:
+            srlRst.append(t)
+    newCandidate = []
+    verbList = ['attend', 'schedule', 'scheduled', 'call', 'make', 'held', 'hold', 'need', 'discuss', 'participate', 'plan', 'take', 'selected', 'join', 'go', 'meet', 'requested', 'reserved', 'bring', 'attending']
+    for i in srlRst:
+        if i.has_key('AM-TMP'):
+            if i.has_key('V') and i['V'] in verbList:
+                newCandidate.append(i['AM-TMP'])
+    if len(newCandidate)!=0:
+        output['when'] = ','.join(newCandidate) + '\t' + ','.join(rstDict['DATE'])
 
+    whereVerbs = ['reserved','booked']
+    if output['where'] == '':
+        loc_candidate = []
+        for i in srlRst:
+            if i.has_key('AM-LOC'):
+                loc_candidate.append(i['AM-LOC'])
+            if i.has_key('A1') and i.has_key('V') and i['V'] in whereVerbs:
+                loc_candidate.append(i['A1'])
+        output['where'] = ','.join(loc_candidate)
+
+
+    if output['who'] == '':
+        output['who'] = data["user_name"]
+    print output
     # Format output to JSON format
     return json.dumps(output)
 
